552ba02f031b3d1d017e9570ea494f67f1611eae
ragam <ragam@DESKTOP-8I1FF0G>
2024-06-27 11:15:07 +0530

    added a new line

--- javaBasics/src/Day1/Sample.java
+++ javaBasics/src/Day1/Sample.java
@@ -6,7 +6,7 @@ public class Sample {
 		System.out.println("Welcome to Full stack Java");
 		System.out.println("MVIT");
 		System.out.println("TNSIF");
-
+		System.out.println("Next line");
 	}
 
 }

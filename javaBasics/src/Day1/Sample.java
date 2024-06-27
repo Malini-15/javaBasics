0baae8023f687f3f6822b1587a21b4dbc755dab1
ragam <ragam@DESKTOP-8I1FF0G>
2024-06-27 11:49:53 +0530

    character project

--- javaBasics/src/Day1/Sample.java
+++ javaBasics/src/Day1/Sample.java
@@ -3,10 +3,8 @@ package Day1;
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Full stack Java");
-		System.out.println("MVIT");
-		System.out.println("TNSIF");
-		System.out.println("Next line");
+		//assigning single character 
+		
 	}
 
 }
